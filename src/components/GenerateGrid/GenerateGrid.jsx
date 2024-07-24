dad2ad25fee352506a6a8096473920ed68d89c90
Lenglet-jeremy <lengletjeremy.pro@gmail.com>
2024-07-24 23:14:27 +0200

    Mise à jour

--- src/components/GenerateGrid/GenerateGrid.jsx
+++ src/components/GenerateGrid/GenerateGrid.jsx
@@ -6,16 +6,19 @@ import { generateCellStyle } from '../../utils/GenerateCellStyle';
 const GenerateGrid = ({ cellValues, initialValues, highlightedCells, checkRules, setCellValues, setHighlightedCells, checkCompletion }) => {
     const tab = [];
 
+
     for (let row = 1; row < 10; row++) {
         for (let col = 1; col < 10; col++) {
             const dataKey = `${row}-${col}`;
             const isHighlighted = highlightedCells[dataKey] || false;
             const isInitialValue = initialValues[dataKey] !== undefined;
             let cellClassName = '';
+
             if (col === 3 || col === 6) cellClassName += 'thick-border-right ';
             if (col === 4 || col === 7) cellClassName += 'thick-border-left ';
             if (row === 3 || row === 6) cellClassName += 'thick-border-bottom ';
             if (row === 4 || row === 7) cellClassName += 'thick-border-top ';
+            
             tab.push(
                 <input
                     key={dataKey}
